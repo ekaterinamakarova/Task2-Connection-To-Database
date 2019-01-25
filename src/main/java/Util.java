2bc09d6ac0d5a18335bc058351757e03d066708f
ekaterinamakarova <ekaterina.makarova.1999@mail.ru>
2019-01-25 10:43:26 +0300

    commit

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Utill {
+public class Util {
 
     private static final String DB_DRIVER="org.postgresql.Driver";
     private static final String DB_URL="jdbc:postgresql://192.168.1.56:5432/dgroops_new";
@@ -14,8 +14,10 @@ public class Utill {
         try {
             Class.forName(DB_DRIVER);
             connection= DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASSWORD);
+            System.out.println("Connection is OK");
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+            System.out.println("ERROR in connection");
         }
         return connection;
     }
